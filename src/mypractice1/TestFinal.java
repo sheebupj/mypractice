6faae07fd109c349e15893e1fe3322d51293231a
Sheebu <Sheebu@paremal>
2022-08-05 19:30:37 +0530

    first commit

---
+++ src/mypractice1/TestFinal.java
@@ -0,0 +1,15 @@
+package mypractice1;
+
+public class TestFinal {
+	
+	
+	public static void main(String[] args) {
+		final int a;
+		a=1;
+		System.out.println("final varibale value="+a );
+		//a=2; once initialised it cannot be changed compiler error
+		
+	}
+	
+
+}
